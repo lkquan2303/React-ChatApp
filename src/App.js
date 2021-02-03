c14fa7f5d80d0b5f505e7960de1f7365e17af149
Quan (Uri) K.LE <uri@enclave.vn>
2021-02-03 16:52:38 +0700

    Implement Chat Engine

--- src/App.js
+++ src/App.js
@@ -1,2 +1,16 @@
-import ChatEngine from 'react-chat-engine'
+import { ChatEngine } from 'react-chat-engine';
 
+import './App.css';
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="0fa4947e-268b-410f-8f8b-7191771772a5"
+      userName="lkquan1609"
+      userSecret="123456"
+    />
+  );
+};
+
+export default App;
